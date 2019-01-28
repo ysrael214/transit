d129eedb7c58833d05b8439cd1b9f7fbb2759bba
Sean Ryan Chan <seanryachan@gmail.com>
2019-01-28 12:28:03 +0800

    Finished Rails coding tasks wrt. Sprint 1

---
+++ Transit/app/controllers/commuters_controller.rb
@@ -0,0 +1,4 @@
+class CommutersController < ApplicationController
+  def new
+  end
+end
---
+++ Transit/db/migrate/20190128033333_create_commuters.rb
@@ -0,0 +1,10 @@
+class CreateCommuters < ActiveRecord::Migration[5.2]
+  def change
+    create_table :commuters do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
