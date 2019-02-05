eb617724d4a7fb6e07865e681de1c17eaef6e447
Menard Ysrael Cruz <ysrael214@gmail.com>
2019-02-05 13:44:46 +0800

    generated TransitLines

---
+++ Transit/app/views/transit_lines/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "transit_lines/transit_line", transit_line: @transit_line
--- Transit/config/routes.rb
+++ Transit/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :transit_lines
   root 'static_pages#home'
   get 'commuters/new'
   resources :favorites
