09ff7231f19b3cce7364029f1e5426ebeafdcad5
Sean Ryan Chan <seanryachan@gmail.com>
2019-02-22 14:55:40 +0800

    seeds, background, some layout changes by Menard

--- Transit/app/helpers/sessions_helper.rb
+++ Transit/app/helpers/sessions_helper.rb
@@ -1,6 +1,22 @@
 module SessionsHelper
+  # session variable is now populated with the corresponding entry.
   def log_in(commuter)
     session[:commuter_id] = commuter.id
+  end
 
+  # Returns the current user, if he/she is logged-in.
+  def current_user
+    if session[:commuter_id]
+      if @commuter.nil?
+        @commuter = Commuter.find_by(id: session[:commuter_id])
+      else
+        @commuter
+      end
+    end
+  end
+
+  # Returns whether user is logged in or not. Exclamation mark to force it as a boolean value.
+  def logged_in?
+    !current_user.nil?
   end
 end
