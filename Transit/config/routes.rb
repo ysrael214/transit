62b27d40461da17f493ef9211af4793afd62ca12
seanryanchan <seanryachan@gmail.com>
2019-03-08 00:45:12 +0800

    added license to routes.rb

--- Transit/config/routes.rb
+++ Transit/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  # License
+  # This is a course requirement for CS 192 Software Engineering II under the supervision of Asst. Prof. Ma. Rowena C. Solamo
+  # of the Department of Computer Science, College of Engineering, University of the Philippines, Diliman for the AY 2018-2019
+
+  # Code History
+  # 1.0 - 2/5/2019 - Sean Ryan Chan - Initial File
+  # 2.0 - 3/4/2019 - Sean Ryan Chan - Transit stops, Transit Lines, Favorites, login, and logout routes.
+
+  # File Creation Date: 2/5/2019
+  # Development Group: Transit Development Tteam (Chan,Cruz,Marrero)
+  # Client Group: UP Student Dormers
+  # Purpose of the Software: The project is to make a web-based application named Transit, and its main vision is to inform dormers and other temporary housed students to travel back to their
+  #   permanent residence of the routes and methods from their current location. Being on a budget, students are more inclined to cheaper options in favor of comfort or travel time.
+
   # For sessions
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
